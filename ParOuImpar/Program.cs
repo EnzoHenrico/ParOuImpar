9c5071c70edf696269212f9cc3dc8fcaf493a8d8
EnzoHenrico <enzo.mascia1@gmail.com>
2024-04-19 00:32:23 -0300

    Add project files.

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,13 @@
+﻿int number;
+
+Console.Write("Digite um numero: ");
+number = int.Parse(Console.ReadLine());
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine("O número é par");
+}
+else
+{
+    Console.WriteLine("O número é impar");
+}
